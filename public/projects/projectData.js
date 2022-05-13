3e92d9944fe8b833555934c9cf342b66ac1ca786
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-05-13 20:36:04 +0530

    profile and projects updation

--- public/projects/projectData.js
+++ public/projects/projectData.js
@@ -1,95 +1,105 @@
 const projects = [
   {
-    Title: "GuruKool",
+    Title: 'Blood Connect',
     Image:
-      "https://raw.githubusercontent.com/pulkit-30/Gurukool/master/gurukool.gif",
-    link: "https://gurukool-c7f70.web.app/",
+      'https://user-images.githubusercontent.com/76155456/167152740-c65ab08b-ae0a-4fc0-9c6c-31a039e669d9.png',
+    link: 'https://blood-connect-94f8e.web.app/',
     description:
-      "Most young grads have a lot of hunger to learn but without any direction, it undermines their effort and some even relatively lose their direction. An easy way to abridge this would be to have a platform or an open application that can bring the people from corporate as well as colleges together.",
-    caption:
-      "A place where you can explore, make network and get guided from the experts.",
-    codeUrl: "https://github.com/pulkit-30/Gurukool",
+      'BloodConnect is an initiative which can help you in finding a person who has the blood group that you need and can really help you in times when someone who wants to, can’t. Wish that no one needs this but in case as you know ideal things does not exist, if anyone needs ,you can be very helpful to that anyone.',
+    caption: 'The Gift of Life',
+    codeUrl: 'https://github.com/pulkit-30/BloodConnect',
   },
   {
-    Title: "Face-Recognition",
+    Title: 'GuruKool',
     Image:
-      "https://i.dailymail.co.uk/i/pix/2014/04/15/article-2605378-1C8E1E4300000578-300_634x455.jpg",
-    link: "https://github.com/pulkit-30/Face-Recognition",
+      'https://raw.githubusercontent.com/pulkit-30/Gurukool/master/gurukool.gif',
+    link: 'https://gurukool-c7f70.web.app/',
     description:
-      "There are two type of detection (in this project) first, image based detection, read image data > train model and recognized faces. second, there is Live face detection, which first read live image data through web cam and train model and recognize different person faces",
+      'Most young grads have a lot of hunger to learn but without any direction, it undermines their effort and some even relatively lose their direction. An easy way to abridge this would be to have a platform or an open application that can bring the people from corporate as well as colleges together.',
+    caption:
+      'A place where you can explore, make network and get guided from the experts.',
+    codeUrl: 'https://github.com/pulkit-30/Gurukool',
+  },
+  {
+    Title: 'Face-Recognition',
+    Image:
+      'https://i.dailymail.co.uk/i/pix/2014/04/15/article-2605378-1C8E1E4300000578-300_634x455.jpg',
+    link: 'https://github.com/pulkit-30/Face-Recognition',
+    description:
+      'There are two type of detection (in this project) first, image based detection, read image data > train model and recognized faces. second, there is Live face detection, which first read live image data through web cam and train model and recognize different person faces',
     caption:
       "Facial recognition is a way of identifying or confirming an individual's identity using their face",
-    codeUrl: "https://github.com/pulkit-30/Face-Recognition",
+    codeUrl: 'https://github.com/pulkit-30/Face-Recognition',
   },
   {
-    Title: "Zipify",
+    Title: 'Zipify',
     Image:
-      "https://raw.githubusercontent.com/pulkit-30/Zipify/master/screenshot.png",
-    link: "https://github.com/pulkit-30/Zipify",
+      'https://raw.githubusercontent.com/pulkit-30/Zipify/master/screenshot.png',
+    link: 'https://github.com/pulkit-30/Zipify',
     description:
-      "A online File compressor which take files from user(upload) and can compress or make zip files",
+      'A online File compressor which take files from user(upload) and can compress or make zip files',
     caption:
-      "Online File Compressor in which anyone can compress and make zip files.",
-    codeUrl: "https://github.com/pulkit-30/Zipify",
+      'Online File Compressor in which anyone can compress and make zip files.',
+    codeUrl: 'https://github.com/pulkit-30/Zipify',
   },
   {
-    Title: "Instagram Clone",
+    Title: 'Instagram Clone',
     Image:
-      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202108/Instagram_0.jpg?ZZLGdE1PjohTO.aeUOUEQYBxAWLPgCGT&size=770:433",
-    link: "https://instagram-clone-app-4f17f.web.app/",
-    description: "Instagram Clone with feed , Post ,comment features",
-    caption: "Instagram Clone with feed , Post ,comment features",
-    codeUrl: "https://github.com/pulkit-30/React-Instagram-clone",
+      'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202108/Instagram_0.jpg?ZZLGdE1PjohTO.aeUOUEQYBxAWLPgCGT&size=770:433',
+    link: 'https://instagram-clone-app-4f17f.web.app/',
+    description: 'Instagram Clone with feed , Post ,comment features',
+    caption: 'Instagram Clone with feed , Post ,comment features',
+    codeUrl: 'https://github.com/pulkit-30/React-Instagram-clone',
   },
   {
-    Title: "Covid-19 Tracker",
+    Title: 'Covid-19 Tracker',
     Image:
-      "https://raw.githubusercontent.com/pulkit-30/Covid-19-Tracker/master/sample.png",
-    link: "https://covid-19-tracker-491fa.firebaseapp.com/",
+      'https://raw.githubusercontent.com/pulkit-30/Covid-19-Tracker/master/sample.png',
+    link: 'https://covid-19-tracker-491fa.firebaseapp.com/',
     description:
-      "This project provide real-time covid-19 cases with Graphical representation and vaccines details using disease.sh api",
+      'This project provide real-time covid-19 cases with Graphical representation and vaccines details using disease.sh api',
     caption:
-      "A react project that track the covid-19 cases worldwide (using disease.sh api)",
-    codeUrl: "https://github.com/pulkit-30/Covid-19-Tracker",
+      'A react project that track the covid-19 cases worldwide (using disease.sh api)',
+    codeUrl: 'https://github.com/pulkit-30/Covid-19-Tracker',
   },
   {
-    Title: "React Blogging",
+    Title: 'React Blogging',
     Image:
-      "https://raw.githubusercontent.com/pulkit-30/React-Blogging/master/pro.gif",
-    link: "https://react-blogging-4c2fc.firebaseapp.com/",
+      'https://raw.githubusercontent.com/pulkit-30/React-Blogging/master/pro.gif',
+    link: 'https://react-blogging-4c2fc.firebaseapp.com/',
     description:
-      "A MERN stack project with User Authentication, Blog Posting, Update User Profile, Editable Blog Posts",
-    caption: "A place where you can publish your blogs",
-    codeUrl: "https://github.com/pulkit-30/React-Blogging",
+      'A MERN stack project with User Authentication, Blog Posting, Update User Profile, Editable Blog Posts',
+    caption: 'A place where you can publish your blogs',
+    codeUrl: 'https://github.com/pulkit-30/React-Blogging',
   },
   {
-    Title: "React Food Order App",
+    Title: 'React Food Order App',
     Image:
-      "https://raw.githubusercontent.com/pulkit-30/React-Food-order-app/master/sample.png",
-    link: "https://react-food-app-d7816.firebaseapp.com/",
+      'https://raw.githubusercontent.com/pulkit-30/React-Food-order-app/master/sample.png',
+    link: 'https://react-food-app-d7816.firebaseapp.com/',
     description:
-      "A MERN stack project, where user can order food online by adding food in his cart and then checkout to place order",
-    caption: "Online Food ordering App with Cart Feature",
-    codeUrl: "https://github.com/pulkit-30/React-Food-order-app",
+      'A MERN stack project, where user can order food online by adding food in his cart and then checkout to place order',
+    caption: 'Online Food ordering App with Cart Feature',
+    codeUrl: 'https://github.com/pulkit-30/React-Food-order-app',
   },
   {
-    Title: "Frontend Games",
+    Title: 'Frontend Games',
     Image:
-      "https://raw.githubusercontent.com/pulkit-30/Frontend-Games/master/poster.png",
-    link: "https://pulkit-30.github.io/Frontend-Games/",
+      'https://raw.githubusercontent.com/pulkit-30/Frontend-Games/master/poster.png',
+    link: 'https://pulkit-30.github.io/Frontend-Games/',
     description:
-      "GitHub Repository with 5 awesome frontend games - Dino-Hunt , Simon Game, Snake Game, Rythm beats, Rock and Roll -  using HTML, CSS , JS , JQuery",
-    caption: "A repository with 5 awesome frontend Games",
-    codeUrl: "https://github.com/pulkit-30/Frontend-Games",
+      'GitHub Repository with 5 awesome frontend games - Dino-Hunt , Simon Game, Snake Game, Rythm beats, Rock and Roll -  using HTML, CSS , JS , JQuery',
+    caption: 'A repository with 5 awesome frontend Games',
+    codeUrl: 'https://github.com/pulkit-30/Frontend-Games',
   },
   {
-    Title: "Frontend Animations",
+    Title: 'Frontend Animations',
     Image:
-      "https://raw.githubusercontent.com/Pulkit203/project-intro-gif/main/Blue%20and%20Yellow%20Modern%20Artisan%20Parties%20and%20Celebrations%20X-Frame%20Banner.gif",
-    link: "https://github.com/pulkit-30/Frontend-Animations",
-    description: "A github repository with awesome frontend animations",
+      'https://raw.githubusercontent.com/Pulkit203/project-intro-gif/main/Blue%20and%20Yellow%20Modern%20Artisan%20Parties%20and%20Celebrations%20X-Frame%20Banner.gif',
+    link: 'https://github.com/pulkit-30/Frontend-Animations',
+    description: 'A github repository with awesome frontend animations',
     caption:
-      "<div class=gradient-text> Stars - 19 </div><div class=gradient-text>forks - 28 </div><div class=gradient-text>  contributors - 26  </div>",
-    codeUrl: "https://github.com/pulkit-30/Frontend-Animations",
+      '<div class=gradient-text> Stars - 19 </div><div class=gradient-text>forks - 28 </div><div class=gradient-text>  contributors - 26  </div>',
+    codeUrl: 'https://github.com/pulkit-30/Frontend-Animations',
   },
 ];
