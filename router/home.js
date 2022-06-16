a7fe531c6e6395fe92929b12e31494b94e3479fc
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-06-16 19:21:37 +0530

    fix: added lazy loading to assets

--- router/home.js
+++ router/home.js
@@ -4,6 +4,7 @@ route.get('/', (req, res) => {
   try {
     ExpModel.find({}, (error, data) => {
       if (error) throw new Error(error);
+      data = data.reverse();
       res.render('index', { title: 'About Pulkit Gupta', exp: data });
     });
   } catch (error) {
